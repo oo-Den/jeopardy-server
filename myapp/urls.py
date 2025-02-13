6f149c81625a93ba3ad75a92e75454fdd5e06ce5
abi19911 <jerome.cwh@gmail.com>
2025-02-14 03:50:22 +0800

    Initialize files (2)
    Co-authored-by: verarr <verarr@fvnki.town>
    Co-authored-by: cosmoere <cosmoere@gmail.com>

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.main, name="board"),
+    path("questions/<int:question_id>/", views.question, name="question")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,2 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from random import randint
+
+def main(request: HttpRequest):
+    rows: int = 5
+    value: int = 200
+    
+    return render(request, "myapp/board-screen.html", {
+        "currency_sign": "$",
+        "players": [
+            {'name': 'irata'},
+            {'name': 'irata'},
+            {'name': 'irata'},
+            {'name': 'irata'},
+        ],
+        "board": {
+            "rows": rows + 1,
+            "columns": [
+                {
+                    "title": f"title {i}",
+                    "cards": [
+                        {
+                            "id": randint(1, 65535),
+                            "value": j
+                        } for j in range(value, value * rows + 1, value)
+                    ]
+                } for i in range(6)
+            ]
+        }
+    })
+
+def question(request: HttpRequest, question_id: int):
+    return render(request, "myapp/question-screen.html", {
+        "players": [
+            {'name': 'irata'},
+            {'name': 'irata'},
+            {'name': 'irata'},
+            {'name': 'irata'},
+        ],
+        "question": {"id": question_id, "text": "Lorem ipsum"}
+    })
