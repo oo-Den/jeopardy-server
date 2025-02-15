c4b6371dd7b4912f735db5c19e308fe3743d3e7b
verarr <verarr@fvnki.town>
2025-02-15 20:35:29 +0100

    make inlines for model relationships

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,10 +3,16 @@ from .models import Board, Column, Card
 
 # Register your models here.
 
+class ColumnInline(admin.TabularInline):
+    model = Board.columns.through
+    verbose_name = "Column"
+    verbose_name_plural = "Columns"
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
     fields = ['name']
     list_display = ['name', 'get_columns', 'get_cards_num']
+
+    inlines = [ColumnInline]
 
     @admin.display(description="Columns")
     def get_columns(self, obj: Board):
@@ -16,10 +22,16 @@ class BoardAdmin(admin.ModelAdmin):
     def get_cards_num(self, obj: Board):
         return sum([col.cards.count() for col in obj.columns.all()])
 
+class CardInline(admin.TabularInline):
+    model = Column.cards.through
+    verbose_name = "Card"
+    verbose_name_plural = "Cards"
 @admin.register(Column)
 class ColumnAdmin(admin.ModelAdmin):
     fields = ['title']
     list_display = ['title', 'get_cards']
+
+    inlines = [CardInline]
 
     @admin.display(description="Cards")
     def get_cards(self, obj: Column):
